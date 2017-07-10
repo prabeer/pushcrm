5ddce6cea1b23bd7dae69eab7727de80e7a57ef6
Administrator <prabeer.kochar@in.panasonic.com>
2017-07-10 15:46:05 +0530

    v1.1

--- Reports.php
+++ Reports.php
@@ -1,5 +1,6 @@
 <?php include_once 'head.php';?>
 <?php include_once 'header.main.php';?>
+<?php include_once 'XLSXWriter/xlsxwriter.class.php';?>
 	<?php
 	
 	include_once 'sidebar.main.php';
@@ -9,7 +10,7 @@
 	} else {
 		$report_type = "";
 	}
-
+	
 	?>
 <div id="content-wrapper" class="content-wrapper view">
 	<div class="container-fluid">
@@ -17,11 +18,20 @@
 		<div id="masonry" class="row">
 			<div
 				class="module-wrapper masonry-item col-lg-9 col-md-9 col-sm-12 col-xs-12">
+				<?php
+				
+if ($report_type == 'IMEI') {
+					$report_type = 'IMEI Wise Package List';
+					$query = 'SELECT distinct(IMEI) IMEI, count(*) package_count FROM weattach_crm.imei_wise_package_list group by IMEI limit 100;';
+					$data = $report_db->query_result ( $query );
+					?>
+				 	
 				<section class="module module-headings">
 					<div class="module-inner">
 						<div class="module-heading">
 							<h3 class="module-title"><?php echo $report_type;?></h3>
-							<ul class="actions list-inline">
+							<ul class="actions list-inline" >
+							<li><a herf="#"><button type="button" class="btn btn-primary">Download</button></a></li>
 								<li><a class="collapse-module" data-toggle="collapse"
 									href="#content-4" aria-expanded="false"
 									aria-controls="content-4"><span aria-hidden="true"
@@ -40,14 +50,18 @@
 										<thead>
 											<tr>
 												<th>#</th>
-												<th>Report Name</th>
-												<th>Report Module</th>
-												<th>Generate</th>
+												<th>IMEI</th>
+												<th>Package Count</th>
 											</tr>
 										</thead>
 										<tbody>
-										
-										
+<?php
+					
+foreach ( $data as $d ) {
+						echo '<tr><td></td><td>' . $d ['IMEI'] . '</td><td>' . $d ['package_count'] . '</td>';
+					}
+					?>
+
 										</tbody>
 									</table>
 								</div>
@@ -59,9 +73,14 @@
 					</div>
 
 				</section>
+				<?php
+				} else {
+					echo 'No report selected';
+				}
+				?>
 
 			</div>
-			
+
 		</div>
 	</div>
 </div>
